28c54793391604de616a428ac52ccf83140fa604
wrightm-mac <wrightm@mac.com>
2017-02-28 13:04:43 +0100

    Initial checkin.

---
+++ ColorMonkey/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ColorMonkey
+//
+//  Created by Michael Wright on 28/02/2017.
+//  Copyright © 2017 wrightm@mac.com. All rights reserved.
+//
+
+import UIKit
+
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
