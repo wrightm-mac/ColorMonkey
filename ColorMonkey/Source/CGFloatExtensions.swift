1b83dadbc2b76a97d70d6a58354cdbf3bd3447ce
wrightm-mac <wrightm@mac.com>
2017-03-03 14:21:47 +0100

    Comments.

--- ColorMonkey/Source/CGFloatExtensions.swift
+++ ColorMonkey/Source/CGFloatExtensions.swift
@@ -14,10 +14,25 @@ public extension CGFloat {
     
     // MARK:    Initialisers...
     
+    /**
+        Create a `CGFloat` based on degrees. Convenience for when creating an
+        **HSL** color.
+     
+        - parameter degrees:    Degrees on the color-wheel. Should be between
+                                `0.0` (representing 1º) and `1.0` (representing
+                                360º).
+    */
     public init(degrees: Int) {
         self = CGFloat(1.0 / 360.0 * Float(degrees))
     }
     
+    /**
+     Create a `CGFloat` based on percentage. Convenience for when creating an
+     **HSL** color.
+     
+     - parameter percent:   Percentage. Should be between
+                            `0.0` (representing 1%) and `1.0` (representing 100º).
+     */
     public init(percent: Int) {
         self = CGFloat(1.0 / 100.0 * Float(percent))
     }
@@ -25,6 +40,9 @@ public extension CGFloat {
     
     // MARK:    Conversions...
     
+    /**
+     `Float` representation.
+    */
     public var float: Float {
         return Float(self)
     }
