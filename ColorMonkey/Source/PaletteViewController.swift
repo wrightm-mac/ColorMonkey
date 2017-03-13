145722ce93bbd28053c58c77167bb94feca20ef0
wrightm-mac <wrightm@mac.com>
2017-03-13 14:40:28 +0100

    Palette tableview.

--- ColorMonkey/Source/PaletteViewController.swift
+++ ColorMonkey/Source/PaletteViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-class PaletteViewController: UIViewController {
+class PaletteViewController: UIViewController, UITableViewDataSource, UITabBarDelegate {
 
     // MARK:    Fields...
     
@@ -37,5 +37,34 @@ class PaletteViewController: UIViewController {
     // MARK:    Navigation...
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+    
+    
+    // MARK:    'UITableViewDataSource'...
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PaletteCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return nil
+    }
+    
+    
+    // MARK:    'UITableViewDelegate'...
+    
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        print("PaletteViewController.\(#function)")
     }
 }
